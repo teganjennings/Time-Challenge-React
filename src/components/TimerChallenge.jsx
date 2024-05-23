207deb39d933b9d6e96f94f333e19299c2a41e95
Tegan Jennings <teganjennings1@hotmail.com>
2024-05-23 20:17:46 +0100

    add: TimerChallenge File

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,18 @@
+export default function TimerChallenge({ title, targetTime }) {
+	function handleStart() {
+		setTimeout(() => {}, targetTime * 1000);
+	}
+	return (
+		<section className="challenge">
+			<h2>{title}</h2>
+			<p className="challenge-time">
+				{targetTime} second{targetTime > 1 ? "s" : ""}
+			</p>
+			<p>
+				{" "}
+				<button>Start Challenge</button>
+			</p>
+			<p className="active">Time is running... / Time inactive</p>
+		</section>
+	);
+}
